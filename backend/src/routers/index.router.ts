6331522de780088da638ff5504d701b33b42ba6a
dev-michaeljohn <dev.michael.john@gmail.com>
2026-08-03 08:47:18 +0800

    renamed "sys" to "protected"

--- backend/src/routers/index.router.ts
+++ backend/src/routers/index.router.ts
@@ -1,10 +1,10 @@
 import { Router, type IRouter } from "express";
 import AuthRouter from "./auth.router.js";
-import SysRouter from "./sys/index.router.js";
+import ProtectedRouter from "./protrected/index.router.js";
 
 const v1Router: IRouter = Router();
 
 v1Router.use("/auth", AuthRouter);
-v1Router.use("/sys", SysRouter);
+v1Router.use("/protected", ProtectedRouter);
 
 export default v1Router;
--- backend/src/routers/protrected/index.router.ts
+++ backend/src/routers/protrected/index.router.ts
@@ -1,5 +1,4 @@
 import AuthController from "@/controllers/auth.controller.js";
-import { authorizeHandler } from "@/middlewares/auth.middleware.js";
 import { createAPIResponse } from "@/utils/response.util.js";
 import { Router, type IRouter, type NextFunction, type Request, type Response } from "express";
 import UserRouter from "./user.router.js";
@@ -10,23 +9,22 @@ import CourseRouter from "./course.route.js";
 import CurriculumRouter from "./curriculum.route.js";
 import ClassRouter from "./class.route.js";
 
-const SysRouter: IRouter = Router();
+const ProtectedRouter: IRouter = Router();
 
-SysRouter.use(AuthController.verifyJWT);
-SysRouter.use(authorizeHandler("SYS_ADMIN"));
+ProtectedRouter.use(AuthController.verifyJWT);
 
-SysRouter.use("/users", UserRouter);
-SysRouter.use("/colleges", CollegeRouter);
-SysRouter.use("/programs", ProgramRouter);
-SysRouter.use("/programs", ProgramRouter);
-SysRouter.use("/semesters", SemesterRouter);
-SysRouter.use("/courses", CourseRouter);
-SysRouter.use("/curriculum", CurriculumRouter);
-SysRouter.use("/classes", ClassRouter);
+ProtectedRouter.use("/users", UserRouter);
+ProtectedRouter.use("/colleges", CollegeRouter);
+ProtectedRouter.use("/programs", ProgramRouter);
+ProtectedRouter.use("/programs", ProgramRouter);
+ProtectedRouter.use("/semesters", SemesterRouter);
+ProtectedRouter.use("/courses", CourseRouter);
+ProtectedRouter.use("/curriculum", CurriculumRouter);
+ProtectedRouter.use("/classes", ClassRouter);
 
-SysRouter.get("/", (_req: Request, res: Response, _next: NextFunction) => {
+ProtectedRouter.get("/", (_req: Request, res: Response, _next: NextFunction) => {
   const response = createAPIResponse(200, "Welcome to Super Admin Routes!.");
   res.status(response.status).json(response);
 });
 
-export default SysRouter;
+export default ProtectedRouter;
