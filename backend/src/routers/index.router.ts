514a6eb4fe94621f43e40ebaf44281ec30cc30ff
dev-michaeljohn <dev.michael.john@gmail.com>
2026-08-24 22:32:44 +0800

    added healthcheck

--- backend/src/routers/index.router.ts
+++ backend/src/routers/index.router.ts
@@ -1,8 +1,14 @@
 import { Router, type IRouter } from "express";
 import AuthRouter from "./auth.router.js";
 import ProtectedRouter from "./protrected/index.router.js";
+import { createAPIResponse } from "@/utils/response.util.js";
 
 const v1Router: IRouter = Router();
+
+v1Router.get("/health", (_req, res, _next) => {
+  const response = createAPIResponse(200, "Hello from PIT-FES V1 API!!");
+  res.status(response.status).json(response);
+});
 
 v1Router.use("/auth", AuthRouter);
 v1Router.use("/protected", ProtectedRouter);
