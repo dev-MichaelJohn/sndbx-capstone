8bf9b42dcebeaed286c3d1c13b7fe2b80b4f43d4
dev-michaeljohn <dev.michael.john@gmail.com>
2026-08-24 19:22:58 +0800

    removed BrowserRouter in AppProvider

--- frontend/src/app/AppProvider.tsx
+++ frontend/src/app/AppProvider.tsx
@@ -1,6 +1,5 @@
 import { type ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter } from "react-router";
 import { ThemeProvider } from "@/components/theme-provider.tsx";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "react-hot-toast";
@@ -19,17 +18,15 @@ export const AppProviders = ({ children }: { children: ReactNode }) => {
   });
 
   return (
-    <BrowserRouter>
-      <ThemeProvider defaultTheme="light">
-        <QueryClientProvider client={queryClient}>
-          <UserProvider>
-            <TooltipProvider>
-              <Toaster position="bottom-right" />
-              {children}
-            </TooltipProvider>
-          </UserProvider>
-        </QueryClientProvider>
-      </ThemeProvider>
-    </BrowserRouter>
+    <ThemeProvider defaultTheme="light">
+      <QueryClientProvider client={queryClient}>
+        <UserProvider>
+          <TooltipProvider>
+            <Toaster position="bottom-right" />
+            {children}
+          </TooltipProvider>
+        </UserProvider>
+      </QueryClientProvider>
+    </ThemeProvider>
   );
 };
